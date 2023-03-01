987f9951ddd3c3b8f74c7a24b2cf95a578bb45f2
Zizhao Hu <zizhaohu3@gmail.com>
2023-03-01 12:00:18 -0800

    latentscatter

--- utils.py
+++ utils.py
@@ -35,10 +35,10 @@ def save_latent_scatter(latent, y):
     for i in range(4):
         for j in range(i,4):
     
-            scatter = plt.subplot(4,4,4*i+j+1).scatter(latent[:,i], latent[:,j], c=y, cmap = 'tab10', s =3)
+            scatter = plt.subplot(4,4,4*i+j+1).scatter(latent[:,j], latent[:,i], c=y, cmap = 'tab10', s =3)
             if j==i:
-                plt.xlabel(f"Latent dimension {i+1}")
-                plt.ylabel(f"Latent dimension {j+1}")
+                plt.xlabel(f"Latent dimension {j+1}")
+                plt.ylabel(f"Latent dimension {i+1}")
             plt.axis('square')
     
     plt.figlegend(*scatter.legend_elements(), loc = 'lower center', ncol=5, labelspacing=0.)
