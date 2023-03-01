59ed1e24d6df76c8a1b061435e73c2aafa7d6fb5
Zizhao Hu <zizhaohu3@gmail.com>
2023-02-28 21:16:48 -0800

    train

---
+++ train.py
@@ -0,0 +1,74 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from model import ConvVAE
+import torchvision.transforms as transforms
+import torchvision
+import matplotlib
+
+from torch.utils.data import DataLoader
+from torchvision.utils import make_grid
+import runner
+from utils import save_reconstructed_images, image_to_vid, save_loss_plot
+
+matplotlib.style.use('ggplot')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# initialize the model
+model = ConvVAE().to(device)
+
+# set the learning parameters
+lr = 0.001
+epochs = 100
+batch_size = 64
+optimizer = optim.Adam(model.parameters(), lr=lr)
+criterion = nn.BCELoss(reduction='sum')
+
+# a list to save all the reconstructed images in PyTorch grid format
+grid_images = []
+
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+])
+# training set and train data loader
+trainset = torchvision.datasets.MNIST(
+    root='/data', train=True, download=True, transform=transform
+)
+trainloader = DataLoader(
+    trainset, batch_size=batch_size, shuffle=True
+)
+# validation set and validation data loader
+testset = torchvision.datasets.MNIST(
+    root='/data', train=False, download=True, transform=transform
+)
+testloader = DataLoader(
+    testset, batch_size=batch_size, shuffle=False
+)
+
+train_loss = []
+valid_loss = []
+for epoch in range(epochs):
+    print(f"Epoch {epoch+1} of {epochs}")
+    train_epoch_loss = runner.train(
+        model, trainloader, trainset, device, optimizer, criterion
+    )
+    valid_epoch_loss, recon_images = runner.validate(
+        model, testloader, testset, device, criterion
+    )
+    train_loss.append(train_epoch_loss)
+    valid_loss.append(valid_epoch_loss)
+    # save the reconstructed images from the validation loop
+    save_reconstructed_images(recon_images, epoch+1)
+    # convert the reconstructed images to PyTorch image grid format
+    image_grid = make_grid(recon_images.detach().cpu())
+    grid_images.append(image_grid)
+    print(f"Train Loss: {train_epoch_loss:.4f}")
+    print(f"Val Loss: {valid_epoch_loss:.4f}")
+
+    # save the reconstructions as a .gif file
+image_to_vid(grid_images)
+# save the loss plots to disk
+save_loss_plot(train_loss, valid_loss)
+print('TRAINING COMPLETE')
