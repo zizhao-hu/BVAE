22f4e6515733503c581df53df57887150113dee9
Zizhao Hu <zizhaohu3@gmail.com>
2023-02-28 21:32:21 -0800

    loss

--- model/ConvVAE.py
+++ model/ConvVAE.py
@@ -95,6 +95,7 @@ class ConvVAE(nn.Module):
     
     def loss(self, x, reconstruction, mu, log_var):
         recon_bce = nn.BCELoss(reduction='sum')(x, reconstruction)
+        print(recon_bce.item())
         var_loss =  -0.5 * torch.sum(1 + log_var - (torch.abs(mu)-self.r).pow(2) - log_var.exp())       
         return recon_bce, var_loss
             
