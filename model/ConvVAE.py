bca9bf62545e76e81ea897704612c98f526b773f
Zizhao Hu <zizhaohu3@gmail.com>
2023-03-05 00:32:32 -0800

    exp1

--- model/ConvVAE.py
+++ model/ConvVAE.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 kernel_size = 4 # (4, 4) kernel
 init_channels = 8 # initial number of filters
 image_channels = 3 
-latent_dim = 16 # latent dimension for sampling
+latent_dim = 32 # latent dimension for sampling
 ###
 
 class ConvVAE(nn.Module):
@@ -33,17 +33,17 @@ class ConvVAE(nn.Module):
             stride=2, padding=1
         )
         self.enc4 = nn.Conv2d(
-            in_channels=init_channels*8, out_channels=64, kernel_size=kernel_size, 
+            in_channels=init_channels*8, out_channels=128, kernel_size=kernel_size, 
             stride=2, padding=0
         )
         # fully connected layers for learning representations
-        self.fc1 = nn.Linear(64, 128)
+        self.fc1 = nn.Linear(128, 128)
         self.fc_mu = nn.Linear(128, latent_dim)
         self.fc_log_var = nn.Linear(128, latent_dim)
-        self.fc2 = nn.Linear(latent_dim, 64)
+        self.fc2 = nn.Linear(latent_dim, 128)
         # decoder 
         self.dec1 = nn.ConvTranspose2d(
-            in_channels=64, out_channels=init_channels*8, kernel_size=kernel_size, 
+            in_channels=128, out_channels=init_channels*8, kernel_size=kernel_size, 
             stride=1, padding=0
         )
         self.dec2 = nn.ConvTranspose2d(
@@ -96,7 +96,7 @@ class ConvVAE(nn.Module):
     
     def decode(self, z):
         z = self.fc2(z)
-        z = z.view(-1, 64, 1, 1)
+        z = z.view(-1, 128, 1, 1)
  
         # decoding
         x = F.relu(self.dec1(z))
@@ -122,6 +122,6 @@ class ConvVAE(nn.Module):
         return reconstruction, mu, log_var
 
 
-# from torchsummary import summary
+from torchsummary import summary
 
-# summary(ConvVAE(), input_size=(3, 64, 64))
+summary(ConvVAE(), input_size=(3, 64, 64))
