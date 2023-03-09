7bb7f5d448933dcf154b64ada6b910207bb45ed4
Zizhao Hu <zizhaohu3@gmail.com>
2023-03-08 19:12:27 -0800

    0.01 test

--- model/HBVAE.py
+++ model/HBVAE.py
@@ -50,8 +50,8 @@ class HBVAE(ConvVAE):
         # agg_logvar =  torch.log(est_mu_01*(1-est_mu_01))
         # est_logvar = torch.zeros_like(agg_logvar).fill_(self.est_logvar)
         sample_mu = 0.1*est_mu+0.9*pri_mu
-        print(max(est_mu),min(est_mu))
-        print(max(sample_mu),min(sample_mu))
+        print(torch.max(est_mu),torch.min(est_mu))
+        print(torch.max(sample_mu),torch.min(sample_mu))
         z = self.reparameterize(sample_mu)
         reconstruction = self.decode(z)
         return reconstruction, pri_mu, agg_mu 
