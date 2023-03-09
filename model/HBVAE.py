c70a842ef17066e0e186b15cc8a140aae3ff801a
Zizhao Hu <zizhaohu3@gmail.com>
2023-03-08 19:24:05 -0800

    0.01 test

--- model/HBVAE.py
+++ model/HBVAE.py
@@ -28,9 +28,6 @@ class HBVAE(ConvVAE):
         eps = torch.bernoulli(mu) # `randn_like` as we need the same size
         return eps
     def loss(self, x, reconstruction, mu, prior_mu=0):
-        print(prior_mu)
-        print(mu)
-
         recon_bce = nn.BCELoss(reduction='sum')(reconstruction, x)
         var_loss = nn.BCELoss(reduction='sum')(mu, prior_mu)
         print(var_loss)
