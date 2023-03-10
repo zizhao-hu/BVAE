de5e8fe6baad950067681f145af9ac2022b9bf41
Zizhao Hu <zizhaohu3@gmail.com>
2023-03-10 15:24:06 -0800

    0.01 test

--- model/HBVAE.py
+++ model/HBVAE.py
@@ -46,7 +46,7 @@ class HBVAE(ConvVAE):
         est_mu, _ = self.encode(x)
         
         est_mu = torch.sigmoid(est_mu)
-        agg_mu = torch.mean(est_mu, dim = ) 
+        agg_mu = torch.mean(est_mu, dim = 1) 
         rep_mu = self.reparameterize(est_mu, 100)
         reconstruction = self.decode(rep_mu)      
         print(est_mu)
